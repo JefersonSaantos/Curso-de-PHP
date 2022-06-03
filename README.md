00f681d4f8fa21109d545c66d6a1b682de866cec
JefersonSaantos <jeff_santos21@hotmail.com>
2022-06-03 12:08:22 -0300

    condicionais

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 Realização de Curso PHP.
+
+Comandos Git
+
+git add .
+
+git commit -m "mensagem do commit"
+
+git push origin main
---
+++ decisoes.php
@@ -0,0 +1,20 @@
+<?php
+
+$idade = 16;
+$numeroDePessoas = 2;
+
+echo "Você só pode entrar, com idade a partir de 18 anos ou ";
+echo "a partir de 16 anos acompanhado." . PHP_EOL;
+
+if ($idade >= 18) {
+    echo "Você tem $idade anos. Pode entrar sozinho!";
+} else if ($idade >= 16 && $numeroDePessoas >1) {
+    echo "Você tem $idade anos, está acompanhado(a), então pode entrar!";
+} else {
+    echo "Você só tem $idade anos. Você não pode entrar";
+}
+
+echo PHP_EOL;
+echo 'Adeus!';
+
+
