c0e9aaaf801e7a377be60a1252a84ae8c60d1f69
JefersonSaantos <jeff_santos21@hotmail.com>
2022-06-08 22:37:52 -0300

    Manipulando Strings

---
+++ strings-inicial/cadastro.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="bootstrap.min.css">
+    <title>Curso Strings</title>
+</head>
+<body>
+
+<?php
+$nomeSobrenome = explode(" ", $_POST['nome'], 2);
+?>
+
+<div class="mx-5 my-5">
+<h1>Cadastro feito com sucesso.</h1>
+<p>Seguem os dados de sua conta:</p>
+<ul class="list-group">
+    <li class="list-group-item">Primeiro nome: <?php echo $nomeSobrenome[0] ?> </li>
+    <li class="list-group-item">Sobrenome: <?php echo $nomeSobrenome[1] ?> </li>
+    <li class="list-group-item">Usuário: </li>
+    <li class="list-group-item">Senha: </li>
+    <li class="list-group-item">Telefone: </li>
+    <li class="list-group-item">Email: </li>
+    <li class="list-group-item">Endereço: </li>
+</ul>
+</div>
+</body>
+</html>
